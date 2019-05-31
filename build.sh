06feafe80b4ba13611400f0cede8eead61da7a66
Matthias Rohmer <rohmer.matthias@anyvent.org>
2019-05-31 20:37:22 +0200

    🐛 Let build command build shared libraries

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-xgo --targets="darwin/amd64,linux/amd64" --pkg transformer github.com/matthiasrohmer/django-amp
+xgo --targets="darwin/amd64,linux/amd64" --pkg transformer --buildmode=c-shared -out amp/lib/transformer github.com/matthiasrohmer/django-amp
