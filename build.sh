49b24f6c587084496d1c8bd3a5ac626851492652
Matthias Rohmer <rohmer.matthias@anyvent.org>
2019-05-31 21:31:02 +0200

    🚧 Update setup and build process

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
+hash go 2>/dev/null || { echo >&2 "Go is required to built django-amp."; exit 1; }
+go get github.com/karalabe/xgo
+
+hash xgo 2>/dev/null || { echo >&2 "xgo could not be found in your $PATH."; exit 1; }
+
 xgo --targets="darwin/amd64,linux/amd64" --pkg transformer --buildmode=c-shared -out amp/lib/transformer github.com/matthiasrohmer/django-amp
--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 import os
-import sys
-import subprocess
-import shutil
 from setuptools import find_packages, setup
-from setuptools.command.install import install
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
@@ -11,54 +7,10 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 # Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-
-# Custom install command class to support building a shared library from Go
-class TransformerInstall(install):
-    def check_version(self):
-        cmd = ['go', 'version']
-        try:
-            subprocess.check_call(cmd, stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError as e:
-            raise Exception(
-                'This package relies on golang 1.5 or newer being installed.')
-
-    def prepare(self):
-        # Clean build directory possibly created in previous runs
-        if os.path.exists('.build'):
-            shutil.rmtree('.build')
-
-        # Setup virtual GOPATH
-        os.makedirs(os.path.join(os.getcwd(), '.build'))
-        os.environ['GOPATH'] = os.path.join(os.getcwd(), '.build')
-
-        # Download dependencies
-        print('Fetching transformer dependencies ...')
-        return subprocess.call('go get'.split())
-
-    def build(self):
-        print('Building transformer ...')
-        cmd = 'go build -a -o amp/vendor/transformer.so -buildmode=c-shared transformer.go'
-        return subprocess.check_call(cmd.split())
-
-    def run(self):
-        self.check_version()
-        try:
-            self.prepare()
-            self.build()
-        except Exception as e:
-            sys.stderr.write(
-                'Could not build transformer needed for django-amp: ' +
-                '{}'.format(e))
-            raise
-        super().run()
-
-
 setup(
     name='django-amp',
-    version='0.1',
-    cmdclass={'install': TransformerInstall},
+    version='0.1b',
     packages=find_packages(),
-    include_package_data=True,
     license='Apache License 2.0',
     description=
     'Template tags and middlewares to make building AMP pages with Django easier.',
@@ -66,6 +18,9 @@ setup(
     url='https://github.com/matthiasrohmer/django-amp',
     author='Matthias Rohmer',
     author_email='rohmer.matthias@anyvent.org',
+    install_requires=[
+        'Django>=2.1',
+    ],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
